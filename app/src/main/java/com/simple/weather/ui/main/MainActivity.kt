efe82d7935d6f281f82ff288e51b5f577a583400
chrisc3456 <chrisc3456@googlemail.com>
2020-04-22 14:14:16 +0100

    View changes to fit under transparent status bar

--- app/src/main/java/com/simple/weather/ui/main/MainActivity.kt
+++ app/src/main/java/com/simple/weather/ui/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.simple.weather.ui.main
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.simple.weather.R
 
@@ -9,5 +10,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set system visibility to overlap the status bar at the top of the screen
+        // colorPrimaryDark set to transparent with child fragments handling insets to adjust which content moves through the status bar on scroll
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN + View.SYSTEM_UI_FLAG_LAYOUT_STABLE
     }
 }
